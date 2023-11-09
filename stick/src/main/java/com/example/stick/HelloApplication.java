cee7e4528ff426078f1412313457583250edb0c7
adityachauhan0 <127509777+adityachauhan0@users.noreply.github.com>
2023-11-10 01:10:41 +0530

    added the home page

--- stick/src/main/java/com/example/stick/HelloApplication.java
+++ stick/src/main/java/com/example/stick/HelloApplication.java
@@ -3,15 +3,19 @@ package com.example.stick;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+        stage.getIcons().add(new Image(Objects.requireNonNull(this.getClass().getResourceAsStream("icons/icon.png"))));
+        stage.setResizable(false);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
