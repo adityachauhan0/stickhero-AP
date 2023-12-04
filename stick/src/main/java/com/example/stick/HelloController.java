e045c12c8f52060dc89280932b3d73c05f9bf399
adityachauhan0 <127509777+adityachauhan0@users.noreply.github.com>
2023-12-04 23:28:30 +0530

    Add files via upload

--- stick/src/main/java/com/example/stick/HelloController.java
+++ stick/src/main/java/com/example/stick/HelloController.java
@@ -7,12 +7,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Bounds;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -27,8 +30,11 @@ public class HelloController implements Initializable {
     private Stage stage;
     @FXML
     private Text cherriesText;
+    @FXML
+    private Text scoretext;
     private Scene scene;
     private Parent parent;
+    private Rectangle lastCollidedRectangle = null;
 
     public void switchToScene2(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("scene2.fxml")));
@@ -121,7 +127,7 @@ public class HelloController implements Initializable {
             myRectangle.setRotate(0.0);
 
             // Bring the rectangle down by the same amount it was increased
-            myRectangle.setY(myRectangle.getY()-initialY + 19); // Adjust the value accordingly
+            myRectangle.setY(myRectangle.getY() - initialY + 19); // Adjust the value accordingly
             myRectangle.setHeight(10);
 
             TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(1), player);
@@ -134,13 +140,64 @@ public class HelloController implements Initializable {
 
             // Play the animation
             translateTransition.play();
-            myRectangle.setX(myRectangle.getX()+myRectangle.getWidth());
-            initialX= myRectangle.getX();
+            myRectangle.setX(myRectangle.getX() + myRectangle.getWidth());
+            initialX = myRectangle.getX();
 
+            // Check for intersections with rectangles that don't have an fx:id in the last 10 pixels from the right
+            for (Node node : ((AnchorPane) myRectangle.getParent()).getChildren()) {
+                if (node instanceof Rectangle && ((Rectangle) node).getFill().equals(javafx.scene.paint.Color.valueOf("#2f2f2f"))) {
+                    String fxId = node.getId();
+                    // Check if the shape has already been scored and is an instance of Rectangle
+                    if (intersectsLast10Pixels(node, myRectangle) && !node.equals(player) && (fxId == null || fxId.isEmpty())) {
+                        if (!((Rectangle) node).isDisable()) {
+                            // Increase scoretext by 1
+                            setScoreText(getScoreText() + 1);
+                            // Disable the shape to prevent scoring multiple times
+                            ((Rectangle) node).setDisable(true);
+                        }
+                    }
+                } else if (node instanceof Rectangle && ((Rectangle) node).getFill().equals(javafx.scene.paint.Color.valueOf("#ff1f1f"))) {
+                    String fxId = node.getId();
+                    // Check if the shape has already been scored and is an instance of Rectangle
+                    if (intersectsLast10Pixels(node, myRectangle) && !node.equals(player) && (fxId == null || fxId.isEmpty())) {
+                        if (!((Rectangle) node).isDisable()) {
+                            // Increase cherries count by 1
+                            setCherriesCount(getCherriesCount() + 1);
+                            setScoreText(getScoreText() + 1);
+                            // Disable the shape to prevent scoring multiple times
+                            ((Rectangle) node).setDisable(true);
+                        }
+                    }
+                }
+            }
+            if (!stickLandedOnPlatform()) {
+                // Stick didn't land on the platform, restart the scene
+                restartScene();
+            }
         }
         myRectangle.setWidth(10.0);
-
     }
+
+    // Add a method to check for intersection in the last 10 pixels from the right
+    private boolean intersectsLast10Pixels(Node shape, Rectangle myRectangle) {
+        double myRectangleRightX = myRectangle.getBoundsInParent().getMaxX();
+        double shapeLast10PixelsRightX = shape.getBoundsInParent().getMaxX() - 10;
+
+        return myRectangleRightX >= shapeLast10PixelsRightX && myRectangle.getBoundsInParent().intersects(shape.getBoundsInParent());
+    }
+
+
+    // Add a method to set the scoretext
+    public void setScoreText(int count) {
+        scoretext.setText(Integer.toString(count));
+    }
+
+    public int getScoreText() {
+        return Integer.parseInt(scoretext.getText());
+    }
+
+
+
 
     // Add a method to set the cherries count
     public void setCherriesCount(int count) {
@@ -150,6 +207,50 @@ public class HelloController implements Initializable {
     public int getCherriesCount() {
         return Integer.parseInt(cherriesText.getText());
     }
+    // Add a method to check if the stick landed on the platform
+    private boolean stickLandedOnPlatform() {
+        AnchorPane anchorPane = (AnchorPane) myRectangle.getParent();
+
+        for (Node node : anchorPane.getChildren()) {
+            if (node instanceof Rectangle && ((Rectangle) node).getFill().equals(Color.web("#2f2f2f")) && !node.equals(myRectangle)) {
+                Bounds platformBounds = node.getBoundsInParent();
+                Bounds stickBounds = myRectangle.getBoundsInParent();
+
+                if (stickBounds.intersects(platformBounds)) {
+                    return true; // Stick collided with a platform
+                }
+            }
+        }
+
+        return false; // Stick didn't collide with any platform
+    }
+
+
+
+    // Add a method to restart the scene
+    // Add a method to restart the scene
+    private void restartScene() {
+        try {
+            // Load the FXML file for the current scene
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("home.fxml"));
+            Parent root = loader.load();
+
+            // Set up the stage and scene
+            stage = (Stage) myRectangle.getScene().getWindow();
+            scene = new Scene(root);
+
+            // Set the controller for the FXML file (adjust the controller class accordingly)
+            HelloController controller = loader.getController();
+            controller.initialize(null, null);
+
+            // Set the scene and show the stage
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle the exception according to your needs
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
